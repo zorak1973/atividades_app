b4813ec4cfb107d55e896a0f21dd8a4f14c8d455
zorak1973 <alexandre.jimenez@gmail.com>
2017-05-28 20:49:25 -0300

    alteracao para bloquear edicao da atividade já concluida

--- DuoSystem/application/views/editar/atividade.php
+++ DuoSystem/application/views/editar/atividade.php
@@ -1,5 +1,12 @@
 <div class="container">
-	<?php echo form_hidden('back', base_url() . 'listar/' . $this->uri->segment(2)); ?>
+	<?php 
+		echo form_hidden('back', base_url() . 'listar/' . $this->uri->segment(2)); 
+		$atrib = "";
+		if($query->status == 4){
+			$atrib = " disabled = 'disabled' ";
+		}
+	
+	?>
 	<?php 
 		$attributes = array('id' =>'FrmEditEst', 'class' =>'form-horizontal', 'method'=>'post', 'role'=>'form'); 
 		
@@ -12,7 +19,7 @@
 		<label for="nome" class="col-sm-2 control-label">Nome</label>
 		<div class="col-sm-8">
 			
-				<?php echo form_input('cadastro[nome]', $query->nome, 'id="nome" maxlength="255" class="validar form-control" validar="digite o nome da atividade"')?>
+				<?php echo form_input('cadastro[nome]', $query->nome, 'id="nome" '.$atrib.' maxlength="255" class="validar form-control" validar="digite o nome da atividade"')?>
 			
 		</div>
 		
@@ -22,7 +29,7 @@
 		<label for="descricao" class="col-sm-2 control-label"><?php echo utf8_encode('Descricão'); ?></label>
 		<div class="col-sm-8">
 			
-				<?php echo form_input('cadastro[descricao]', $query->descricao, 'id="descricao"  class="form-control" validar="digite a descricao" ')?>
+				<?php echo form_input('cadastro[descricao]', $query->descricao, 'id="descricao" '.$atrib.' class="form-control" validar="digite a descricao" ')?>
 			
 		</div>
 	</div>
@@ -32,11 +39,11 @@
 		<label for="dataIni" class="col-sm-2 control-label">Data Inicial</label>
 		<div class="col-sm-2">
 			
-			<?php echo form_input('cadastro[data_inicio]', $query->data_inicio, 'id="date" class="form-control" maxlength="10" readonly="readonly"');?>
+			<?php echo form_input('cadastro[data_inicio]', $query->data_inicio, 'id="date" '.$atrib.' class="form-control" maxlength="10" readonly="readonly"');?>
 		</div>
 		<label for="data_fim" class="col-sm-2 control-label">Data Final</label>
 		<div class="col-sm-2">
-			<?php echo form_input('cadastro[data_fim]', $query->data_fim, 'id="date2" class="form-control" maxlength="10" readonly="readonly"');?>
+			<?php echo form_input('cadastro[data_fim]', $query->data_fim, 'id="date2" '.$atrib.' class="form-control" maxlength="10" readonly="readonly"');?>
 		</div>
 	</div>
 	
@@ -44,18 +51,18 @@
 	<div class="form-group">
 		<label for="status" class="col-sm-2 control-label">Status</label>
 			<div class="col-sm-2">
-				<?php echo form_dropdown('cadastro[status]', $status, $query->status, 'id="status" class="form-control"')?>
+				<?php echo form_dropdown('cadastro[status]', $status, $query->status, 'id="status" '.$atrib.' class="form-control"')?>
 			</div>
 		<label for="situacao" class="col-sm-2 control-label"><?php echo utf8_encode('Situação'); ?></label>
 			<div class="col-sm-2">	
-				<?php echo form_dropdown('cadastro[situacao]', $situacao, $query->situacao,'id="situacao" class="form-control"')?>
+				<?php echo form_dropdown('cadastro[situacao]', $situacao, $query->situacao,'id="situacao" '.$atrib.' class="form-control"')?>
 			</div>
 	</div>		
 	<hr />
 	<div class="col-md-offset-8">
 	<div class="btn-group" role="group">
 			<?php echo form_button('', 'Voltar', 'class="redirect btn btn-primary" redirect="' . base_url() . 'listar/' . $this->uri->segment(2) . '"');?>
-			<button type="submit" class="btn btn-primary" id="sub">Enviar</button>
+			<button type="submit" class="btn btn-primary" <?php echo $atrib; ?> id="sub">Enviar</button>
 		
 	</div>
 	</div>
@@ -65,6 +72,7 @@
 	
 	
 </div>	
+
 
 		
 		
